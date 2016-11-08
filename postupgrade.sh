06defa68d38c5b2d06e0ba50b214014e2e05f75f
Loxberry <loxberry@loxberry.fritz.box>
2016-11-08 06:41:25 +0100

    Backup during Upgrade

--- postupgrade.sh
+++ postupgrade.sh
@@ -7,14 +7,17 @@ ARGV3=$3 # Third argument is Plugin installation folder
 ARGV4=$4 # Forth argument is Plugin version
 ARGV5=$5 # Fifth argument is Base folder of LoxBerry
 
-#echo "<INFO> Copy back existing config files"
-#cp -p -v -r /tmp/$ARGV1\_upgrade/config/$ARGV3/* $ARGV5/config/plugins/$ARGV3/ 
+echo "<INFO> Copy back existing config files"
+cp -p -v -r /tmp/$ARGV1\_upgrade/config/$ARGV3/* $ARGV5/config/plugins/$ARGV3/ 
 
-#echo "<INFO> Copy back existing log files"
-#cp -p -v -r /tmp/$ARGV1\_upgrade/log/$ARGV3/* $ARGV5/log/plugins/$ARGV3/ 
+echo "<INFO> Copy back existing log files"
+cp -p -v -r /tmp/$ARGV1\_upgrade/log/$ARGV3/* $ARGV5/log/plugins/$ARGV3/ 
 
-#echo "<INFO> Remove temporary folders"
-#rm -r /tmp/$ARGV1\_upgrade
+echo "<INFO> Copy back existing data files"
+cp -p -v -r /tmp/$ARGV1\_upgrade/data/$ARGV3/* $ARGV5/data/plugins/$ARGV3/ 
+
+echo "<INFO> Remove temporary folders"
+rm -r /tmp/$ARGV1\_upgrade
 
 # Exit with Status 0
 exit 0
--- preupgrade.sh
+++ preupgrade.sh
@@ -7,17 +7,20 @@ ARGV3=$3 # Third argument is Plugin installation folder
 ARGV4=$4 # Forth argument is Plugin version
 ARGV5=$5 # Fifth argument is Base folder of LoxBerry
 
-#echo "<INFO> Creating temporary folders for upgrading"
-#mkdir -p /tmp/$ARGV1\_upgrade
-#mkdir -p /tmp/$ARGV1\_upgrade/config
-#mkdir -p /tmp/$ARGV1\_upgrade/log
-#mkdir -p /tmp/$ARGV1\_upgrade/files
+echo "<INFO> Creating temporary folders for upgrading"
+mkdir -p /tmp/$ARGV1\_upgrade
+mkdir -p /tmp/$ARGV1\_upgrade/config
+mkdir -p /tmp/$ARGV1\_upgrade/log
+mkdir -p /tmp/$ARGV1\_upgrade/data
 
-#echo "<INFO> Backing up existing config files"
-#cp -p -v -r $ARGV5/config/plugins/$ARGV3/ /tmp/$ARGV1\_upgrade/config
+echo "<INFO> Backing up existing config files"
+cp -p -v -r $ARGV5/config/plugins/$ARGV3/ /tmp/$ARGV1\_upgrade/config
 
-#echo "<INFO> Backing up existing log files"
-#cp -p -v -r $ARGV5/log/plugins/$ARGV3/ /tmp/$ARGV1\_upgrade/log
+echo "<INFO> Backing up existing log files"
+cp -p -v -r $ARGV5/log/plugins/$ARGV3/ /tmp/$ARGV1\_upgrade/log
+
+echo "<INFO> Backing up existing data files"
+cp -p -v -r $ARGV5/log/plugins/$ARGV3/ /tmp/$ARGV1\_upgrade/data
 
 # Exit with Status 0
 exit 0
